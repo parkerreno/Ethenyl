f9424c8cd681b63af57856f39c867256d35e31bf
Parker Ciambrone <parkerreno@msn.com>
2016-05-14 18:41:01 -0700

    Haha I'm a mess...  I'll figure out the project structure eventually.

---
+++ Ethenyl/SoftServe/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SoftServe.MvcApplication" Language="C#" %>
--- Ethenyl/WaferCone/WaferCone.UWP/Properties/AssemblyInfo.cs
+++ Ethenyl/WaferCone/WaferCone.UWP/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WaferCone")]
+[assembly: AssemblyTitle("WaferCone.UWP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WaferCone")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("WaferCone.UWP")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
