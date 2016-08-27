bd2a24314d0181acbea97321d3eb62a8a0442869
Parker Ciambrone <parkerreno@msn.com>
2016-08-27 01:04:40 -0700

    Fiddling with the assembly info... not like it matters.

--- Ethenyl/SoftServe/Properties/AssemblyInfo.cs
+++ Ethenyl/SoftServe/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SoftServe")]
+[assembly: AssemblyTitle("Project Ethenyl - SoftServe")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SoftServe")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("Project Ethenyl - SoftServe")]
+[assembly: AssemblyCopyright("Copyright © Parker Ciambrone/ ParkerReno.net 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
