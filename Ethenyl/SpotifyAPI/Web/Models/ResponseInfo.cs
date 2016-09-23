13e7abcaea87b4cc4497ba78ba35b168ea64b32e
ParkerReno <parkerreno@msn.com>
2016-09-23 16:10:26 -0700

    Updated spotify api library (was hitting a null reference exception)

---
+++ Ethenyl/SpotifyAPI/Web/Models/ResponseInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace SpotifyAPI.Web.Models
+{
+    public class ResponseInfo
+    {
+        public WebHeaderCollection Headers { get; set; }
+
+        public static readonly ResponseInfo Empty = new ResponseInfo();
+    }
+}
